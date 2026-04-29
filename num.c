5be4b13b362465e73bc9c76c3958f9079216c2fb
sreeyajinitha2006bci27-design <sreeyajinitha2006_bci27@mepcoeng.ac.in>
2026-04-29 12:37:46 +0530

    Create num.c

---
+++ num.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void swap(int *n1, int *n2) {
+    int temp = *n1;
+    *n1 = *n2;
+    *n2 = temp;
+}
+
+int main() {
+    int num1 = 5, num2 = 10;
+    swap(&num1, &num2);
+    printf("num1 = %d\nnum2 = %d", num1, num2);
+    return 0;
+}
